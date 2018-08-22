64654560f8bb9d26084f368f8c4d5e8895ffc56c
Abhiraj Singh <abhi973686@gmail.com>
2018-08-23 02:47:32 +0530

    Assignment 23 Aug

---
+++ staticjava.java
@@ -0,0 +1,20 @@
+//static block is executed before Constructors!!!
+class test
+{
+  static int i;
+  static
+  {
+    System.out.println("Static is called");
+  }
+  test()
+  {
+    System.out.println("Constructor is called"); 
+  }
+}
+class staticjava
+{
+  public static void main(String[] args) {
+    test a=new test();
+    
+  }
+}
